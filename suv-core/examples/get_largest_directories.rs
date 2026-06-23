757ae4c0640d74c10c5ec4860955aa538717521b
Zarif Atai <z.atai@protonmail.com>
2026-06-24 01:27:30 +0200

    Rewrite get_largest_directories logic

---
+++ suv-core/examples/get_largest_directories.rs
@@ -0,0 +1,5 @@
+use suv_core::get_largest_directories;
+
+fn main() {
+    let _ = get_largest_directories(Some(5), Some("/"));
+}
--- suv-core/src/lib.rs
+++ suv-core/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, path::Path};
 pub use sysinfo::{Disk, Disks};
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 pub fn get_mount_point_disk<'a>(disks: &'a Disks, mount_point: Option<&Path>) -> Option<&'a Disk> {
     let default_mount_point = "/";
@@ -18,28 +18,28 @@ pub fn get_largest_directories(
     let n_directories = n_directories.unwrap_or(5);
     let root = root.unwrap_or("/");
 
-    let mut top_directories = HashMap::new();
-
     for entry in WalkDir::new(root).max_depth(1) {
-        if let Ok(entry) = entry {
-            let path = entry.path().display().to_string();
-            let storage_size = entry.metadata().unwrap().len();
-            if top_directories.len() as u8 <= n_directories {
-                top_directories.insert(path, storage_size);
-            } else {
-                if let Some((key, value)) = top_directories.iter().min_by_key(|(_, v)| v) {
-                    if storage_size > *value {
-                        top_directories.remove(key);
-                        top_directories.insert(path, storage_size);
-                    }
+        if let Ok(x) = entry {
+            if let Ok(metadata) = x.metadata() {
+                let mut storage_size = metadata.len();
+                if metadata.is_dir() {
+                    storage_size += WalkDir::new(x.path())
+                        .into_iter()
+                        .filter_map(|x| x.ok())
+                        .filter_map(|x| x.metadata().ok())
+                        .map(|x| x.len())
+                        .sum();
+                } else if metadata.is_symlink() {
+                    todo!();
                 }
             }
         }
     }
+
     Ok(())
 }
 
-struct StorageEntry {
+struct DirSummary {
     path: String,
     storage_size: u64,
 }
