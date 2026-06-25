d84892f2277b8cb8b3c2987033d35362665cb0c1
Zarif Atai <z.atai@protonmail.com>
2026-06-26 00:25:57 +0200

    Completed logic for getting top N largest directories

--- suv-core/examples/get_largest_directories.rs
+++ suv-core/examples/get_largest_directories.rs
@@ -1,5 +1,12 @@
-use suv_core::get_largest_directories;
+use suv_core::get_directories_sizes;
 
 fn main() {
-    let _ = get_largest_directories(Some(5), Some("/"));
+    let n_top_directories = 5;
+    let directories = get_directories_sizes(Some("/"));
+
+    if let Ok(mut dirs) = directories {
+        dirs.sort_by_key(|&(_, size)| size);
+        let top_directories: Vec<_> = dirs.iter().rev().take(n_top_directories).collect();
+        println!("Top directories:\n{top_directories:?}");
+    }
 }
--- suv-core/src/lib.rs
+++ suv-core/src/lib.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, path::Path};
+use std::path::{Path, PathBuf};
 pub use sysinfo::{Disk, Disks};
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
 
 pub fn get_mount_point_disk<'a>(disks: &'a Disks, mount_point: Option<&Path>) -> Option<&'a Disk> {
     let default_mount_point = "/";
@@ -11,35 +11,32 @@ pub fn get_mount_point_disk<'a>(disks: &'a Disks, mount_point: Option<&Path>) ->
         .find(|disk| disk.mount_point() == mount_point)
 }
 
-pub fn get_largest_directories(
-    n_directories: Option<u8>,
-    root: Option<&str>,
-) -> Result<(), walkdir::Error> {
-    let n_directories = n_directories.unwrap_or(5);
-    let root = root.unwrap_or("/");
+pub fn get_directories_sizes(root: Option<&str>) -> Result<Vec<(PathBuf, u64)>, walkdir::Error> {
+    let default_root = "/";
+
+    let root = root.unwrap_or(default_root);
+    let mut directories_sizes: Vec<(PathBuf, u64)> = Vec::new();
 
     for entry in WalkDir::new(root).max_depth(1) {
         if let Ok(x) = entry {
             if let Ok(metadata) = x.metadata() {
                 let mut storage_size = metadata.len();
                 if metadata.is_dir() {
-                    storage_size += WalkDir::new(x.path())
+                    let total_size_dir: u64 = WalkDir::new(x.path())
                         .into_iter()
                         .filter_map(|x| x.ok())
                         .filter_map(|x| x.metadata().ok())
                         .map(|x| x.len())
                         .sum();
+
+                    storage_size += total_size_dir;
+                    directories_sizes.push((x.path().to_path_buf(), storage_size));
                 } else if metadata.is_symlink() {
-                    todo!();
+                    break;
                 }
             }
         }
     }
 
-    Ok(())
-}
-
-struct DirSummary {
-    path: String,
-    storage_size: u64,
+    Ok(directories_sizes)
 }
