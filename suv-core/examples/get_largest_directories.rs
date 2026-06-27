29701d675eadceb0bad3ab850ac37e403e0cec7d
Zarif Atai <z.atai@protonmail.com>
2026-06-27 16:16:55 +0200

    Remove old example cod

--- suv-core/examples/get_largest_directories.rs
+++
@@ -1,9 +0,0 @@
-use suv_core::get_directories_sizes;
-
-fn main() {
-    let n_top_directories = 5;
-    let mut directories = get_directories_sizes(Some("/"));
-    directories.sort_by_key(|&(_, size)| size);
-    let top_directories: Vec<_> = directories.iter().rev().take(n_top_directories).collect();
-    println!("Top directories:\n{top_directories:?}");
-}
